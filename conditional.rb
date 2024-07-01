1e8a40f556958115f3f7f286e02bafa6663affcd
Ganeshkm2407 <ganeshkm2402@gmail.com>
2024-07-01 14:47:59 +0530

    conditionals and loops

---
+++ conditional.rb
@@ -0,0 +1,65 @@
+#numbers = [1, 2, 3,]
+
+# def max(num1, num2, num3)
+#     if num1 >= num2 and num1 >= num3
+#         return num1
+#     elsif num2 >= num1 and num2 >= num3
+#         return num2
+#     else
+#         return num3
+# end
+# puts max (numbers[0],numbers[1],numbers[2])
+
+# #iteration
+# # for loop in array
+# collection = ["one", "two", "three"]
+# for item in collection
+#     puts item
+# end
+# collection.each do |item|
+#     puts item
+# end
+# #for loop in hash
+# ages = {
+#   "Alice" => 30,
+#   "Bob" => 25,
+#   "Charlie" => 35,
+#   "David" => 20
+# }
+# puts "each method:"
+# ages.each do |name, age|
+#   puts "#{name} is #{age} years old." if age >= 30
+# end
+# # while loop
+# a=10
+# i = 0
+# while i<=a do
+# puts "hi hello"
+# i += 1
+# end
+
+# unless condition
+num = 10
+unless num < 0
+    puts "#{num} is Positive Number."
+else
+    puts "#{num} is Negative Number."
+end
+
+if num < 0
+    puts "#{num} is Positive Number."
+else
+    puts "#{num} is Negative Number."
+end
+
+=begin
+
+var = 7
+ 
+# using until loop
+until var == 11 do
+    puts var * 10
+    var = var + 1
+end
+=end
+puts "hi"
